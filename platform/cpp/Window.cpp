2ebb1de2ec8db9f686bb58d18259471cebfd4cd9
Łukasz Pończocha <lukasz.ponczocha@gmail.com>
2019-02-10 13:09:33 +0100

    Changed X max pin value, moved main.cpp file to another location due to Windows filenames restriction

--- platform/cpp/Window.cpp
+++ platform/cpp/Window.cpp
@@ -21,7 +21,7 @@
 #define X_STEP_PIN 2
 #define X_DIR_PIN 0
 #define X_MIN_PIN 25
-#define X_MAX_PIN 26
+#define X_MAX_PIN 4//26
 #define X_ENABLE_PIN 3
 
 #define Y_STEP_PIN 13
--- platform/cpp/main.cpp
+++ platform/cpp/main.cpp
@@ -1,35 +1,17 @@
-#include "commands.hpp"
+#include "Window.hpp"
 
-int main() {
-  ifstream fin;
-  vector<string> token;
-  int n = 0;
-  Command::vector_SPtr vCommands;
-  Parser tokenizer;
-  fin.open("gcode.txt");
-  if(!fin.good())
-    return 1;
 
-  while(true) {
-    try {
-      tokenizer.parser(fin, n);
-    }
-    catch(string error) {
-      cout << error << n+1 << endl;
-      exit(1);
-    }
-    if(fin.eof()) break;
-    token = tokenizer.getCopyOfVector();
-    vCommands.push_back(Factory::getCommand(token[0],n)); 
-    vCommands[n]->validate(token, n); 
-    vCommands[n]->createCommand(token);
-    n++;
-  }
- 
-  for(Command::SPtr& command: vCommands) {
-    command->execute();
-  }
-  return 0;
+int main(int argc, char *argv[])
+{
+  
+  Gtk::Main kit(argc, argv);
+  Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("Builder.ui");
+  Window *okno = 0;
+  
+  builder -> get_widget_derived("window1", okno);
+  
+  kit.run(*okno);
+  
 }
 
 
